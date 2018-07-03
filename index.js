68b90f1df47a9bd0f4122064cefb99aae0125da9
mohsen mahv <mohsen.mahv@gmail.com>
2018-07-03 19:45:50 +0430

    fix(glbl-npm):complete code and set it running

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 const {exec} = require('child_process');
 
-//linux
-exec('which npm', (err, stdout) => {
-    if (err) {
-        console.error('---', err);
-        return;
-    }
-    exec(stdout.replace('\n', '').replace('\r', '') + ' -v', (err, stdout) => {
-        if (err) {
-            console.log('---', err);
-            return;
-        }
-        console.log(stdout);
-    });
-});
+function npmAddress(stdout) {
+    return stdout
+        .substr(0, stdout.indexOf('\n') > 0 ? stdout.indexOf('\n') : stdout.length)
+        .substr(0, stdout.indexOf('\n') > 0 ? stdout.indexOf('\r') : stdout.length);
+}
 
+function getGlobalNpmPath() {
+    return new Promise((resolve, reject) => {
+        exec('which npm', (err1, stdout) => {
+            if (err1) {
+                exec('where npm', (err2, stdout) => {
+                    if (err2) {
+                        reject({err1, err2});
+                    }
+                    resolve(npmAddress(stdout));
+                });
+                return;
+            }
+            resolve(npmAddress(stdout));
+        });
+    });
+}
+
+function wrapInString(input) {
+    return `"${input}"`;
+}
+
+function logExecResult(err, stdout) {
+    if (err) {
+        throw err;
+    }
+
+    console.log(stdout);
+}
+
+function npm(args) {
+    getGlobalNpmPath().then(result => {
+        let npmPath = wrapInString(result);
+        console.log('executing', npmPath + ' ' + args);
+        exec(npmPath + ' ' + args, logExecResult)
+    });
+}
+
+console.log('node version:');
+npm('-v');
+
+exports = npm;
